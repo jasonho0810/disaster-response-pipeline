e41900e5828484b3a8f25d5ed0b10d4532ed7648
jasonho0810 <jasonho0810@gmail.com>
2020-05-17 19:12:35 -0400

    Initial commit

---
+++ models/train_classifier.py
@@ -0,0 +1,106 @@
+import sys
+import pandas as pd
+import numpy as np
+import pickle
+from sqlalchemy import create_engine
+
+import re
+import nltk
+nltk.download(['punkt', 'wordnet', 'stopwords', 'averaged_perceptron_tagger', 'maxent_ne_chunker'])
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk import pos_tag
+from nltk import ne_chunk
+
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+
+def load_data(database_filepath):
+    # load data from database
+    engine = create_engine('sqlite:///'+database_filepath)
+    df = pd.read_sql_table('disaster', con = engine)
+    X = df['message']
+    cat_names = df.columns.values[4:]
+    Y = df[cat_names]
+    
+    return X, Y, cat_names
+
+
+def tokenize(text):
+    # Normalize text
+    text = re.sub(r"(\d)[^a-zA-Z0-9_](\d)", "\\1\\2", text.lower())
+    text = re.sub(r"[^a-zA-Z0-9_]", " ", text)
+    
+    # Tokenize text
+    tokens = word_tokenize(text)
+    
+    # Remove stop words
+    tokens = [word for word in tokens if word not in stopwords.words('english')]
+    
+    # Reduce words to their root form
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).strip()
+        clean_tokens.append(clean_tok)
+    
+    return clean_tokens
+
+
+def build_model():
+    model = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('rfclf', MultiOutputClassifier(RandomForestClassifier(n_jobs = -1, random_state = 42), n_jobs = -1)), 
+    ])
+    
+    return model
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    Y_pred = model.predict(X_test)
+    for i, col in enumerate(Y_test[category_names]):
+        print(col)
+        print(classification_report(Y_test[col], Y_pred[:,i], target_names=['yes', 'no']))
+
+
+def save_model(model, model_filepath):
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
